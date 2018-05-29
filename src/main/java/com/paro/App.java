7b102d39a10652fb99d8ffbe5503387121698030
Parvathygk <parvathygk.26@gmail.com>
2018-05-29 12:24:02 +0530

    first commit

---
+++ src/main/java/com/paro/App.java
@@ -0,0 +1,13 @@
+package com.paro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
